dcef7edc88023a0a814934729dbe600b507f6ef0
Matt Gray <804029+MattGrayYes@users.noreply.github.com>
2025-08-18 01:08:58 +0100

    Checks filenames based on content-disposition header, removes existing files that aren't part of this gallery

--- icloud_photo.sh
+++ icloud_photo.sh
@@ -5,15 +5,18 @@
 # arg 1: iCloud web album URL
 # arg 2: folder to download into (optional)
 
+echo -e "\n* iCloud Photo Downloader *\n"
+
 function curl_post_json {
 	curl -sH "Content-Type: application/json" -X POST -d "@-" "$@"
 }
 
-printf "Getting iCloud Stream\n"
+printf "Connecting to iCloud Shared Album\n"
 BASE_API_URL="https://p23-sharedstreams.icloud.com/$(echo $1 | cut -d# -f2)/sharedstreams"
 
-pushd $2 2> /dev/null
+pushd $2 > /dev/null
 STREAM=$(echo '{"streamCtag":null}' | curl_post_json "$BASE_API_URL/webstream")
+
 HOST=$(echo $STREAM | jq '.["X-Apple-MMe-Host"]' | cut -c 2- | rev | cut -c 2- | rev)
 
 if [ "$HOST" ]; then
@@ -21,10 +24,14 @@ if [ "$HOST" ]; then
     STREAM=$(echo '{"streamCtag":null}' | curl_post_json "$BASE_API_URL/webstream")
 fi
 
-printf "Grabbing Large File Checksums\n"
+
+echo "$(echo "$STREAM" | jq -r '.userFirstName + " " +.userLastName +": " + .streamName')"
+echo ""
+
+# Grabbing Large File Checksums
 CHECKSUMS=$(echo $STREAM | jq -r '.photos[] | [(.derivatives[] | {size: .fileSize | tonumber, value: .checksum})] | max_by(.size | tonumber).value')
 
-printf "Adding Checksums to Array\n"
+# Adding Checksums to Array
 for CHECKSUM in $CHECKSUMS; do
     arrCHKSUM+=($CHECKSUM)
 done
@@ -32,14 +39,11 @@ printf "Total Downloads: ${#arrCHKSUM[@]}\n"
 
 # Dedup checksum to only include unique ids.
 arrCHKSUM=($(printf "%s\n" "${arrCHKSUM[@]}" | sort -u))
-printf "Unique Downloads: ${#arrCHKSUM[@]}\n"
+printf "Unique Downloads: ${#arrCHKSUM[@]}\n\n"
 
-printf "Streaming All Assets\n"
-echo $STREAM \
-| jq -c "{photoGuids: [.photos[].photoGuid]}" \
-| curl_post_json "$BASE_API_URL/webasseturls" \
-| jq -r '.items | to_entries[] | "https://" + .value.url_location + .value.url_path + "&" + .key' \
-| while read URL; do
+FILENAMES=()
+
+while read URL; do
 
 	# Get this URL's checksum value, not all URL's will be downloaded as there are both the fill size AND the thumbnail link in the Assets stream.
 	LOCAL_CHECKSUM=$(echo "${URL##*&}")
@@ -51,32 +55,62 @@ echo $STREAM \
 			# then we must delimit again on ? to remove all the URL parameters after the filename.
 			# Example: https://www.example.com/4/5/IMG_0828.JPG?o=param1&v=param2&z=param3....
 			FILE=$(echo $URL|cut -d "/" -f6 | cut -d "?" -f1)
-            echo $FILE
+            
+            HEADER=$(curl -s --range 0-0 -D - "$URL" -o /dev/null)
+            FILENAME=$(echo "$HEADER" | awk -F'filename=' '/[Cc]ontent-[Dd]isposition/ {gsub(/[";\r]/, "", $2); print $2; exit}')
+            FILENAMES+=("$FILENAME")
 
 			# Don't download movies
-			if [[ "$FILE" == *.mp4* ]]; then
+			if [[ "$FILENAME" == *.mp4* ]]; then
 				echo "Downloading movie"
 					curl -OJ $URL
 			else
 
 				# Don't download files that already exist
-				if [[ -f "$FILE" ]]; then
-					printf "File $FILE already present. Ignoring \n"
+				if [[ -f "$FILENAME" ]]; then
+					printf "Ignoring: $FILENAME\n"
 					#TIMESTAMP=$(date +%s%N)
 					#curl $URL -o "${TIMESTAMP}_${FILE}"
 
 				else
-					# Original curl -sOJ $URL -> s = silent : O = download to file : J = Save using uploaded filename -- this also skips files that already exist.
-					curl -OJ $URL
+					# s = silent : O = download to file : J = Save using uploaded filename -- this also skips files that already exist.
+					printf "Downloading: $FILENAME\n"
+                    curl -sOJ $URL
 				fi
 
 			fi
 
-	else
-		echo "Skipping Thumbnail"
+	#else
+		#echo "Skipping Thumbnail"
 	fi
 
+done < <(
+    echo "$STREAM" \
+    | jq -c '{photoGuids: [.photos[].photoGuid]}' \
+    | curl_post_json "$BASE_API_URL/webasseturls" \
+    | jq -r '.items | to_entries[] | "https://" + .value.url_location + .value.url_path + "&" + .key'
+)
+
+echo -e "\nChecking for unexpected files in download directory"
+
+set +e
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+# If the image file is not in the known list of filenames, delete it
+for IMAGE_FILE in *; do
+    if ! containsElement "$IMAGE_FILE" "${FILENAMES[@]}"; then
+        echo "Deleting unexpected file: $IMAGE_FILE"
+        rm $IMAGE_FILE
+    fi
 done
 
-popd 2> /dev/null
+echo -e "iCloud Photo Downloader Finished\n"
+
+
+popd > /dev/null
 wait
